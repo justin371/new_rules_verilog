d2a1c042c0fe763a80bfde128aa2e1b44e84a3e7
Lijie Wang <lijie.wang@xztech.ai>
2026-07-21 16:53:43 +0800

    Revert "Allow standalone VCS lint interface tops"
    This reverts commit 4d71077bc29dae348ade9a0fafac9690cafea06b.

--- vendors/synopsys/verilog_rtl_lint_default_opts.f
+++ vendors/synopsys/verilog_rtl_lint_default_opts.f
@@ -1,6 +1,4 @@
 # Default VCS lint options for verilog_rtl_lint_test.
-# Projects can override the rulefile_vcs attribute to replace this policy.
+# Projects can override the rulefile attribute to add or replace policy.
 +lint=all,noVCDE
 +warn=all
-# Standalone lint tops intentionally leave their external interface ports open.
--suppress=SV-UIP
